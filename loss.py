af2cf8e24908d76f9f0fdb5a489856b87e2a72ea
alok <kurdleak.alok@gmail.com>
2018-04-30 09:01:48 +0530

    All losses added

--- loss.py
+++ loss.py
@@ -1,6 +1,6 @@
 import cupy as cp
 import chainer.functions as F
-import numpy as np
+
 
 
 def gradient_loss(generated, truth):
@@ -15,8 +15,20 @@ def gradient_loss(generated, truth):
     d_tx = F.convolution_2d(truth, wx)
     d_ty = F.convolution_2d(truth, wy)
 
-    return F.sum(F.absolute(d_gx - d_tx)) + F.sum(F.absolute(d_gy - d_ty))
+    return (F.sum(F.absolute(d_gx - d_tx)) + F.sum(F.absolute(d_gy - d_ty))) / n
+
 
 
 def l2_loss(generated, truth):
-    return F.sum(F.squared_difference(generated, truth))
+    n, c, h, w = generated.shape
+    return (F.sum(F.squared_difference(generated, truth))) / n
+
+def loss_target1(dis_output):
+    n, _ = dis_output.shape
+    return -F.sum(F.log(dis_output)) / n
+
+
+
+def loss_target0(dis_output):
+    n, _ = dis_output.shape
+    return -F.sum(F.log(1 - dis_output)) / n
